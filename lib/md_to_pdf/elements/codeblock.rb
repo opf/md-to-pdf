655c9211e58b040c7fce579efe4d1d37606c389b
as-op <a.sandorf@openproject.com>
2023-06-05 18:40:39 +0200

    feat(codeblock): create a table row per line instead of one large cell

--- lib/md_to_pdf/elements/codeblock.rb
+++ lib/md_to_pdf/elements/codeblock.rb
@@ -2,23 +2,38 @@ module MarkdownToPDF
   module Codeblock
     def draw_codeblock(node, opts)
       cell_style_opts = codeblock_style(opts)
-      cell = build_codeblock_cell(node, cell_style_opts)
+      table_rows = build_codeblock_cell_rows(node, cell_style_opts)
       with_block_margin_all(codeblock_margin_style) do
-        @pdf.table([[cell]], width: @pdf.bounds.right, cell_style: cell_style_opts)
+        @pdf.table(table_rows, width: @pdf.bounds.right, cell_style: cell_style_opts) do
+          if table_rows.length > 1
+            cells.columns(0).rows(0..0).each { |cell| cell.padding_bottom = 1 }
+            cells.columns(0).rows(-1..-1).each { |cell| cell.padding_top = 1 }
+            value_cells = cells.columns(0).rows(1..-2)
+            value_cells.each do |cell|
+              cell.padding_top = 1
+              cell.padding_bottom = 1
+            end
+          end
+        end
       end
     end
 
     private
 
-    def build_codeblock_cell(node, cell_style_opts)
-      text = node.string_content.gsub(' ', Prawn::Text::NBSP)
+    def build_codeblock_cell_rows(node, cell_style_opts)
+      lines = node.string_content.gsub(' ', Prawn::Text::NBSP).split("\n")
+      lines.map do |line|
+        [build_codeblock_cell(line, cell_style_opts)]
+      end
+    end
+
+    def build_codeblock_cell(text, cell_style_opts)
       Prawn::Table::Cell::Text.new(
         @pdf, [0, 0],
         content: text,
         font: cell_style_opts[:font],
         size: cell_style_opts[:size],
-        text_color: cell_style_opts[:color],
-        borders: cell_style_opts[:borders]
+        text_color: cell_style_opts[:color]
       )
     end
 
