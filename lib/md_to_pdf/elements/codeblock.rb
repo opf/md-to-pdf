90eca7cc00dd3640687cd4d579919b833ab3fcde
Andrej Sandorf <77627197+as-op@users.noreply.github.com>
2024-06-24 09:56:04 +0200

    mermaid-cli support (#6)
    * support for mermaid if mermaid-cli is installed

--- lib/md_to_pdf/elements/codeblock.rb
+++ lib/md_to_pdf/elements/codeblock.rb
@@ -1,6 +1,11 @@
+require "open3"
+require "tempfile"
+
 module MarkdownToPDF
   module Codeblock
     def draw_codeblock(node, opts)
+      return if node.fence_info == "mermaid" && build_mermaid_block(node, opts)
+
       cell_style_opts = codeblock_style(opts)
       table_rows = build_codeblock_cell_rows(node, cell_style_opts)
       return if table_rows.empty?
@@ -21,6 +26,68 @@ module MarkdownToPDF
     end
 
     private
+
+    def mermaid_cli_available?
+      return $mermaid_cli_available if defined?($mermaid_cli_available)
+
+      $mermaid_cli_available =
+        begin
+          _, status = Open3.capture2e("mmdc", "-V")
+          status.success?
+        rescue StandardError
+          false
+        end
+    end
+
+    def build_mermaid_block(node, _opts)
+      return false unless respond_to?(:mermaid_cli_enabled?) && mermaid_cli_enabled?
+      return false unless mermaid_cli_available?
+
+      image_file = Tempfile.new(['mermaid', ".png"])
+      begin
+        return false unless run_mermaid_cli(node.string_content, image_file.path, 'png',
+                                            { background: "transparent", theme: "neutral" })
+
+        image_obj, image_info = @pdf.build_image_object(image_file.path)
+        options = {
+          scale: [@pdf.bounds.width / image_info.width.to_f, 1].min
+        }
+        @pdf.embed_image(image_obj, image_info, options)
+        true
+      rescue StandardError
+        false
+      ensure
+        image_file.unlink
+      end
+    end
+
+    def run_mermaid_cli(mmdc, destination, format, options = {})
+      tmp_file('mermaid', 'mmdc', mmdc) do |filename|
+        args = ["-i", filename, '-e', format, '-o', destination]
+        args << '-C' << options[:css_file] if options[:css_file]
+        args << '-t' << options[:theme] if options[:theme]
+        args << '-w' << options[:width] if options[:width]
+        args << '-H' << options[:height] if options[:height]
+        args << '-s' << options[:scale] if options[:scale]
+        args << '-b' << options[:background] if options[:background]
+        args << '-p' << options[:puppeteer_config_file] if options[:puppeteer_config_file]
+        args << '-c' << options[:config_file] if options[:config_file]
+        _, status = Open3.capture2e("mmdc", *args)
+        status.success?
+      rescue StandardError
+        false
+      end
+    end
+
+    def tmp_file(basename, input_ext, code)
+      source_file = Tempfile.new([basename, ".#{input_ext}"])
+      begin
+        File.write(source_file.path, code)
+        yield source_file.path
+      ensure
+        source_file.unlink
+      end
+    end
 
     def build_codeblock_cell_rows(node, cell_style_opts)
       lines = node.string_content.gsub(' ', Prawn::Text::NBSP).split("\n")
